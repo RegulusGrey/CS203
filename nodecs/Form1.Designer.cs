8a26e9f067d83391df33b25b244c0c5aaa3bc2cc
RegulusGrey <108450056+RegulusGrey@users.noreply.github.com>
2023-05-05 16:16:23 +0800

    ctrl grpbox creation

--- nodecs/Form1.Designer.cs
+++ nodecs/Form1.Designer.cs
@@ -41,10 +41,21 @@
             textBox2 = new TextBox();
             adjacentNodeBtn = new Button();
             textBox3 = new TextBox();
-            button1 = new Button();
+            destinationBtn = new Button();
             textBox4 = new TextBox();
-            button2 = new Button();
+            srcBtn = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label3 = new Label();
+            groupBox3 = new GroupBox();
             menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -54,7 +65,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, algorithmToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(969, 28);
+            menuStrip1.Size = new Size(1244, 28);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -104,11 +115,11 @@
             // 
             // createNodeBtn
             // 
-            createNodeBtn.BackColor = SystemColors.InactiveBorder;
+            createNodeBtn.BackColor = SystemColors.GradientInactiveCaption;
             createNodeBtn.FlatStyle = FlatStyle.Popup;
-            createNodeBtn.Location = new Point(831, 79);
+            createNodeBtn.Location = new Point(67, 37);
             createNodeBtn.Name = "createNodeBtn";
-            createNodeBtn.Size = new Size(105, 40);
+            createNodeBtn.Size = new Size(301, 40);
             createNodeBtn.TabIndex = 1;
             createNodeBtn.Text = "Create Node";
             createNodeBtn.UseVisualStyleBackColor = false;
@@ -116,85 +127,175 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(759, 86);
+            textBox1.Location = new Point(33, 59);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(43, 27);
+            textBox1.Size = new Size(101, 27);
             textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(759, 143);
+            textBox2.Location = new Point(232, 59);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(43, 27);
+            textBox2.Size = new Size(101, 27);
             textBox2.TabIndex = 4;
             // 
             // adjacentNodeBtn
             // 
-            adjacentNodeBtn.BackColor = SystemColors.InactiveBorder;
+            adjacentNodeBtn.BackColor = SystemColors.GradientInactiveCaption;
             adjacentNodeBtn.FlatStyle = FlatStyle.Popup;
-            adjacentNodeBtn.Location = new Point(831, 136);
+            adjacentNodeBtn.Location = new Point(33, 114);
             adjacentNodeBtn.Name = "adjacentNodeBtn";
-            adjacentNodeBtn.Size = new Size(105, 40);
+            adjacentNodeBtn.Size = new Size(300, 40);
             adjacentNodeBtn.TabIndex = 3;
             adjacentNodeBtn.Text = "Adjacent";
             adjacentNodeBtn.UseVisualStyleBackColor = false;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(759, 566);
+            textBox3.Location = new Point(307, 47);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(43, 27);
             textBox3.TabIndex = 8;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
-            // button1
+            // destinationBtn
             // 
-            button1.BackColor = SystemColors.InactiveBorder;
-            button1.FlatStyle = FlatStyle.Popup;
-            button1.Location = new Point(831, 559);
-            button1.Name = "button1";
-            button1.Size = new Size(105, 40);
-            button1.TabIndex = 7;
-            button1.Text = "Adjacent";
-            button1.UseVisualStyleBackColor = false;
+            destinationBtn.BackColor = SystemColors.GradientInactiveCaption;
+            destinationBtn.FlatStyle = FlatStyle.Popup;
+            destinationBtn.Location = new Point(208, 30);
+            destinationBtn.Name = "destinationBtn";
+            destinationBtn.Size = new Size(93, 61);
+            destinationBtn.TabIndex = 7;
+            destinationBtn.Text = "End";
+            destinationBtn.UseVisualStyleBackColor = false;
+            destinationBtn.Click += destinationBtn_Click;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(759, 509);
+            textBox4.Location = new Point(117, 47);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(43, 27);
             textBox4.TabIndex = 6;
+            textBox4.TextChanged += textBox4_TextChanged;
             // 
-            // button2
+            // srcBtn
             // 
-            button2.BackColor = SystemColors.InactiveBorder;
-            button2.FlatStyle = FlatStyle.Popup;
-            button2.Location = new Point(831, 502);
-            button2.Name = "button2";
-            button2.Size = new Size(105, 40);
-            button2.TabIndex = 5;
-            button2.Text = "Create Node";
-            button2.UseVisualStyleBackColor = false;
+            srcBtn.BackColor = SystemColors.GradientInactiveCaption;
+            srcBtn.FlatStyle = FlatStyle.Popup;
+            srcBtn.Location = new Point(18, 30);
+            srcBtn.Name = "srcBtn";
+            srcBtn.Size = new Size(93, 61);
+            srcBtn.TabIndex = 5;
+            srcBtn.Text = "Start";
+            srcBtn.UseVisualStyleBackColor = false;
+            srcBtn.Click += srcBtn_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(33, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Edge 1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(232, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Edge 2";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ActiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(35, 296);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(365, 285);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = SystemColors.GradientActiveCaption;
+            groupBox1.Controls.Add(adjacentNodeBtn);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Location = new Point(35, 98);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(365, 178);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Connect Edges";
+            groupBox1.Enter += groupBox1_Enter_1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.GradientActiveCaption;
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Controls.Add(destinationBtn);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(srcBtn);
+            groupBox2.Location = new Point(35, 599);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(365, 196);
+            groupBox2.TabIndex = 12;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Path";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Traversal:";
+            // 
+            // groupBox3
+            // 
+            groupBox3.BackColor = SystemColors.GradientActiveCaption;
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Controls.Add(groupBox2);
+            groupBox3.Controls.Add(createNodeBtn);
+            groupBox3.Controls.Add(groupBox1);
+            groupBox3.Location = new Point(805, 50);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(427, 812);
+            groupBox3.TabIndex = 13;
+            groupBox3.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.InactiveBorder;
-            ClientSize = new Size(969, 652);
-            Controls.Add(textBox3);
-            Controls.Add(button1);
-            Controls.Add(textBox4);
-            Controls.Add(button2);
-            Controls.Add(textBox2);
-            Controls.Add(adjacentNodeBtn);
-            Controls.Add(textBox1);
-            Controls.Add(createNodeBtn);
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(1244, 874);
+            Controls.Add(groupBox3);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "net";
+            Load += Form1_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -214,8 +315,15 @@
         private TextBox textBox2;
         private Button adjacentNodeBtn;
         private TextBox textBox3;
-        private Button button1;
+        private Button destinationBtn;
         private TextBox textBox4;
-        private Button button2;
+        private Button srcBtn;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label3;
+        private GroupBox groupBox3;
     }
 }
--- nodecs/Form1.cs
+++ nodecs/Form1.cs
@@ -7,9 +7,51 @@ namespace nodecs
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void createNodeBtn_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void srcBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void destinationBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
         }
     }
 }
